75119252c9e1f6441510f7e502d258a37f7afb0a
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2021-07-05 07:43:04 +0530

    aded signup

--- src/Components/Screens/Signup.js
+++ src/Components/Screens/Signup.js
@@ -1,9 +1,73 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import '../../Styles/Signup.css'
+import '../../Styles/index.css'
+import svg from '../../assets/signup.svg'
+import Tilt from 'react-parallax-tilt';
 function Signup() {
+    const [name, setName] = useState("")
+    const [mail, setMail] = useState("")
+    const [mobile, setMobile] = useState()
+    const [password, setPassword] = useState("")
     return (
-        <div>
-            Signup
+        <div className="signup-container">
+            <div className="signup-flex">
+                <div>
+                    <div>
+                        <div className="text-large">Create Account</div>
+                        <div className="text-small">Connect with everyone in the CIT</div>
+                    </div>
+                    <div>
+                        <form>
+                            <div className="flex-4">
+                                <div className="form-item">
+                                    <label for="first-name">Name</label><br />
+                                    <input name="first-name" spellCheck='false' value={name} onChange={(e) => setName(e.target.value)} />
+                                </div>
+                                <div className="form-item">
+                                    <label for="first-name">Dayscholars/Hosteller</label><br />
+                                    <input name="first-name" spellCheck='false' value="name" onChange={(e) => setName(e.target.value)} />
+                                </div>
+                            </div>
+                            <div className="flex-4">
+                                <div className="form-item">
+                                    <label for="first-name">E-mail</label><br />
+                                    <input name="first-name" spellCheck='false' value={mail} onChange={(e) => setMail(e.target.value)} />
+                                </div>
+                                <div className="form-item">
+                                    <label for="first-name">Mobile</label><br />
+                                    <input name="first-name" spellCheck='false' value={mobile} onChange={(e) => setMobile(e.target.value)} />
+                                </div>
+                            </div>
+                            <div className="form-item" >
+                                <label for="first-name" >Password</label><br />
+                                <input name="first-name" type="Password" className="pass" spellCheck='false' value={password} onChange={(e) => setPassword(e.target.value)} />
+                            </div>
+                            <div className="sigup-button">
+                                <button className="green-solid-button form-item" style={{ margin: "20px" }}>Sign up</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div className="svg-content">
+                    <div className="flex-2">
+                        <div className="flex-3">
+                            <br /><br /><br /><br /><br />
+                            <div className="text-medium">Already a member?</div>
+                            <div>
+                                <button className="button-green-outline">LOGIN</button>
+                            </div>
+                        </div>
+                        <Tilt>
+                            <img src={svg} className="svg" alt="svg" />
+                        </Tilt>
+                    </div>
+                </div>
+            </div>
+            <div style={{ textAlign: "center" }} className="already">
+                <div>Already a member?<>Login</></div>
+            </div>
+
         </div>
     )
 }
